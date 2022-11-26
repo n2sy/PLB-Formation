7e4f47c2406cadb0fe30c839ffc1f9eb4ec634ab
nidhal <nidhal.jelassi@fsegt.utm.tn>
2022-11-26 21:03:55 +0100

    change api url

--- backend/app.js
+++ backend/app.js
@@ -2,17 +2,22 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cvRoutes = require('./routes/cv');
 const authRoutes = require('./routes/auth');
+var cors = require('cors');
+var fs = require("fs");
+var https = require("https");
 const app = express();
 
 app.use(express.json()); //application/json
 
-app.use((req, res, next) => {
-    //res.setHeader('Access-Control-Allow-Origin', 'https://cdpn.io')
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-})
+app.use(cors())
+
+// app.use((req, res, next) => {
+//     //res.setHeader('Access-Control-Allow-Origin', 'https://cdpn.io')
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//     next();
+// })
 
 
 
@@ -30,7 +35,14 @@ app.use((error, req, res, next) => {
 mongoose.connect('mongodb+srv://nidhal:azerty@cvproject.cpsst.mongodb.net/projectDB?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true
-}).then(result => app.listen(3000, console.log("Server started !")))
+}).then(result => https
+    .createServer(
+        {
+            key: fs.readFileSync("server.key"),
+            cert: fs.readFileSync("server.cert"),
+        },
+        app
+    ).listen(3000, console.log("Server started !")))
     .catch(err => console.log(err));
 // mongoose.connect('mongodb://localhost:27017/Laposte3', {
 //     useNewUrlParser: true,
